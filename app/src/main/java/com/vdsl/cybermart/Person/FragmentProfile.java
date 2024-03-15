2cd1c46c4ab199a44237f39ca6660e19d00e8e3a
Dang Van Si <dangvansi1405@gmail.com>
2024-03-16 00:57:50 +0700

    16/03/24: #1 open activity Category management

--- app/src/main/java/com/vdsl/cybermart/Person/FragmentProfile.java
+++ app/src/main/java/com/vdsl/cybermart/Person/FragmentProfile.java
@@ -1,5 +1,6 @@
 package com.vdsl.cybermart.Person;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,18 +10,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.vdsl.cybermart.R;
+import com.vdsl.cybermart.Person.View.CategoryManagementActivity;
+import com.vdsl.cybermart.databinding.FragmentProfileBinding;
 
 public class FragmentProfile extends Fragment {
+    private FragmentProfileBinding profileBinding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container,false);
+        profileBinding = FragmentProfileBinding.inflate(inflater, container, false);
+        return profileBinding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        profileBinding.cvCateManage.setOnClickListener(v -> {
+            startActivity(new Intent(getContext(), CategoryManagementActivity.class));
+        });
     }
 }
